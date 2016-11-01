4dee0f6b5baed2f14d5965dfb9d037470917f65c
Chad Moyer <chad.moyer@trumbullcorp.com>
2016-11-01 16:01:59 -0400

    Added functions for each set of data I want to collect and a generic write function to write to a text file

--- ExtraLifeAlerts.py
+++ ExtraLifeAlerts.py
@@ -4,35 +4,61 @@ import time
 
 #####################################################################
 # Global variables to customize for your own donations.
-participantID = 196451
-textFileFolders = ""
-teamID = 27277
+participant_id = 196451
+text_file_folders = ""
+team_id = 27277
 #####################################################################
 
 # URLs to pull data
-participantInfoURL = "http://www.extra-life.org/index.cfm?fuseaction=donordrive.participant&participantID=" + str(participantID) + "&format=json"
-participantDonationInfoURL = "http://www.extra-life.org/index.cfm?fuseaction=donordrive.participantDonations&participantID=" + str(participantID) + "&format=json"
-teamInfoURL = "http://www.extra-life.org/index.cfm?fuseaction=donorDrive.team&teamID=" + str(teamID) + "&format=json"
+participant_info_url = "http://www.extra-life.org/index.cfm?fuseaction=donordrive.participant&participantID=" + str(participant_id) + "&format=json"
+participant_donation_info_url = "http://www.extra-life.org/index.cfm?fuseaction=donordrive.participantDonations&participantID=" + str(participant_id) + "&format=json"
+team_info_url = "http://www.extra-life.org/index.cfm?fuseaction=donorDrive.team&teamID=" + str(team_id) + "&format=json"
+
+
+def write_to_text_file(data, filename):
+    f = open(text_file_folders + filename, 'w')
+    f.write(data)
+    f.close()
+
+def latest_donation(json):
+    latest_donation_amount = str(json[0]['donorName'] + " - " + "$" + json[0]['donationAmount'])
+    write_to_text_file(latest_donation_amount, "LastDonation.txt")
+
+def top_donation(json):
+
+
+def my_total_raised(json):
+
+
+def team_total_raised(json):
+
+
 
 
 while True:
 
     # Get the Participant Info JSON object
-    participantInfoJSON = urllib.request.urlopen(participantInfoURL)
+    participantInfoJSON = urllib.request.urlopen(participant_info_url)
     tmpParticipantInfoResponse = participantInfoJSON.read().decode('utf-8')
     participantInfoObj = json.loads(tmpParticipantInfoResponse)
 
     # Get the Participant Donation Info JSON object
-    participantDonationInfoJSON = urllib.request.urlopen(participantDonationInfoURL)
+    participantDonationInfoJSON = urllib.request.urlopen(participant_donation_info_url)
     tmpParticipantDonationInfoResponse = participantDonationInfoJSON.read().decode('utf-8')
     participantDonationInfoObj = json.loads(tmpParticipantDonationInfoResponse)
 
     # Get the Team Info JSON object
-    teamInfoJSON = urllib.request.urlopen(teamInfoURL)
+    teamInfoJSON = urllib.request.urlopen(team_info_url)
     tmpTeamInfoResponse = teamInfoJSON.read().decode('utf-8')
     teamInfoObj = json.loads(tmpTeamInfoResponse)
 
+    # Write the data to our text files
+    latestdonation(participantDonationInfoJSON)
+    topdonation(participantDonationInfoJSON)
+    mytotalraised(participantInfoJSON)
+    teamtotalraised(teamInfoJSON)
 
+    time.sleep(60)
 
     #print(participantInfoObj)
 
